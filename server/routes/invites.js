b5b4ffbd1bea2bbbed076987f911ed1bb50aceae
JaskaranBadhan <150879404+JaskaranBadhan@users.noreply.github.com>
2025-12-23 17:55:33 +0530

    Add files via upload

---
+++ server/routes/invites.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middlewares/authMiddleware');
+const {
+    inviteUser,
+    getMyInvites,
+    acceptInvite,
+    rejectInvite,
+    getGroupInvites
+} = require('../controllers/inviteController');
+router.post('/', protect, inviteUser);
+router.get('/my', protect, getMyInvites);
+router.post('/:id/accept', protect, acceptInvite);
+router.post('/:id/reject', protect, rejectInvite);
+router.get('/group/:groupId', protect, getGroupInvites);
+module.exports = router;
